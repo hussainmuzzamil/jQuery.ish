7fa134072f7733e2fbaba1163a8f297b92617d38
Bill Shackelford <bshackelford@gmail.com>
2012-04-19 09:47:01 -0400

    add rename

---
+++ query.ish.js
@@ -0,0 +1,46 @@
+var $ = function(id){
+	var o =  new $.fn.init(id).elm				
+	for(key in $.fn)
+		o[key] = $.fn[key]
+	return o
+}
+$.fn = {
+	init : function(string) {
+		$.fn.elm=document.querySelectorAll(string)								
+		return this;
+	},
+	hasClass : function(cl){
+		for(var i = 0; i < $.fn.elm.length; i++){
+			var objCl = $.fn.elm[i].className.split(' ');
+			for (var ii in objCl) {
+				if (objCl[ii] == cl) {
+					return true;
+				}
+			}
+		}
+		return false;
+	},
+	addClass : function(cl){
+		for(var i = 0; i < $.fn.elm.length; i++)
+			$.fn.elm[i].className = ($.fn.elm[i].className + " " + cl).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+		return this;
+	},
+	toggleClass : function(cl){
+		for(var i = 0; i < $.fn.elm.length; i++)
+			this.hasClass(cl) ? this.removeClass(cl) : this.addClass(cl)						
+		return this
+	},
+	removeClass : function(cl){
+		for(var i = 0; i < $.fn.elm.length; i++){
+			var objCl = $.fn.elm[i].className.split(' ')
+			for(var ii = 0; ii < objCl.length; ii++){
+				if (objCl[ii] == cl) {
+					delete(objCl[ii]);
+				}
+			}
+			$.fn.elm[i].className = objCl.join(' ').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+		}	
+		return this;
+	}
+};
+$.fn.init.prototype = $.fn;
